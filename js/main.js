27f7c4c4d12dfb6259993ef98b314585341414fe
ali qomi <46956808+aliqomi@users.noreply.github.com>
2022-10-20 11:38:13 +0330

    Add2

--- js/main.js
+++ js/main.js
@@ -1,54 +1,50 @@
-const comments = [
-  {
-    name: 'Mario',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At.',
-    children: [
-      {
-        name: 'Emily',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, velit iste architecto placeat doloribus repellat! Asperiores numquam ratione voluptatum reiciendis?',
-        children: [
-          {
-            name: 'Mario',
-            text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, necessitatibus?',
-            children: [
-              {
-                name: 'Emily',
-                text: 'Lorem, ipsum dolor.',
-                children: [],
-              },
-            ],
-          },
-          {
-            name: 'David',
-            text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, necessitatibus?',
-            children: [],
-          },
-        ]
-      }
-    ],
-  },
-  {
-    name: 'John',
-    text: 'Lorem ipsum dolor sit amet.'
-  }
+const comments = [{
+        name: 'Mario',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. At.',
+        children: [{
+            name: 'Emily',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, velit iste architecto placeat doloribus repellat! Asperiores numquam ratione voluptatum reiciendis?',
+            children: [{
+                    name: 'Mario',
+                    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, necessitatibus?',
+                    children: [{
+                        name: 'Emily',
+                        text: 'Lorem, ipsum dolor.',
+                        children: [],
+                    }, ],
+                },
+                {
+                    name: 'David',
+                    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt, necessitatibus?',
+                    children: [],
+                },
+            ]
+        }],
+    },
+    {
+        name: 'John',
+        text: 'Lorem ipsum dolor sit amet.'
+    }
 ];
 
 function renderComments(comments) {
-  let output = '';
+    let output = '';
 
-  for (const comment of comments) {
-    const name = `<b>${comment.name}</b>`;
-    const text = `<p>${comment.text}</p>`;
-    let children = '';
+    for (const comment of comments) {
+        const name = `<b>${comment.name}</b>`;
+        const text = `<p>${comment.text}</p>`;
+        let child = '';
 
-    if (Array.isArray(comment.children) && comment.children.length > 0) {
-      children = renderComments(comment.children);
+        if (Array.isArray(comment.children) && comment.children.length > 0) {
+            child = renderComments(comment.children);
+        }
+
+        //output += `<li>${name}${text}${child}</li>`;
+        output = output + `<li>${name}${text}${child}</li>`;
     }
 
-    output += `<li>${name}${text}${children}</li>`;
-  }
-
-  return `<ul>${output}</ul>`;
+    return `<ul>${output}</ul>`;
+    //return '<ul>' + output + '</ul>'
 }
 
 document.querySelector('.comments-container').innerHTML = renderComments(comments);
